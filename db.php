8e2afee0289ebffb35a8ee030166938eed9d0dd6
Rinat Khaziev <rinat.khaziev@gmail.com>
2022-09-28 11:49:10 -0500

    Simpler check

--- db.php
+++ db.php
@@ -374,7 +374,7 @@ class hyperdb extends wpdb {
 		} elseif ( is_array( $args ) ) {
 			// 8.0+ changed the behavior of call_user_func_array(), associative arrays would turn into named attributes
 			// Here we discard the keys and hope for the best
-			if ( version_compare( PHP_VERSION, '8.0.0', '>=' ) ) {
+			if ( PHP_MAJOR_VERSION >= 8 ) {
 				$args = array_values( $args );
 			}
 
