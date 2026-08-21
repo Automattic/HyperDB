241770eaec930fe519bf78c83a199ba622dcf7ae
Rebecca Hum <rebeccahum@gmail.com>
2026-08-21 13:38:19 -0600

    Route SELECT FOR UPDATE queries to writable servers

--- db.php
+++ db.php
@@ -340,8 +340,14 @@ class hyperdb extends wpdb {
 	 * @return bool
 	 */
 	public function is_write_query( $q ) {
-		// Quick and dirty: only SELECT statements are considered read-only.
 		$q = ltrim( $q, "\r\n\t (" );
+
+		// Locking reads require a writable server, even though they begin with SELECT.
+		if ( preg_match( '/^SELECT\s.*\bFOR\s+UPDATE\b/is', $q ) ) {
+			return true;
+		}
+
+		// Quick and dirty: only SELECT statements are considered read-only.
 		return ! preg_match( '/^(?:SELECT|SHOW|DESCRIBE|DESC|EXPLAIN)\s/i', $q );
 	}
 
